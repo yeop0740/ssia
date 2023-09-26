efb17d860afab8d092ffa7e90afc58a49cdef72e
yeop0740 <ssucgoo@gmail.com>
2023-09-26 17:57:54 +0900

    [feat] PasswordEncoder 빈 등록

---
+++ ssia-ch6-ex1/src/main/java/com/example/ssiach6ex1/PasswordConfig.java
@@ -0,0 +1,21 @@
+package com.example.ssiach6ex1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SCryptPasswordEncoder sCryptPasswordEncoder() {
+        return new SCryptPasswordEncoder();
+    }
+
+}
