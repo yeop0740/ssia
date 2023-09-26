b9760ad193ae0dcb816543d2fba7bb0727e3735c
yeop0740 <ssucgoo@gmail.com>
2023-09-27 02:10:16 +0900

    [feat] 인증 없이 모든 엔드포인트에 접근할 수 있도록 설정

--- ssia-ch7-ex1/src/main/java/com/example/ssiach7ex1/config/ProjectConfig.java
+++ ssia-ch7-ex1/src/main/java/com/example/ssiach7ex1/config/ProjectConfig.java
@@ -2,6 +2,8 @@ package com.example.ssiach7ex1.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -9,7 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class ProjectConfig {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -34,6 +36,14 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+
+        http.authorizeHttpRequests()
+                .anyRequest().permitAll();
     }
 
 }
