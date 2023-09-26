f4ab950456787e0b381a1714f7c06bc67d65cb39
yeop0740 <ssucgoo@gmail.com>
2023-09-27 02:05:52 +0900

    [feat] PasswordEncoder 등록

--- ssia-ch7-ex1/src/main/java/com/example/ssiach7ex1/config/ProjectConfig.java
+++ ssia-ch7-ex1/src/main/java/com/example/ssiach7ex1/config/ProjectConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -27,6 +29,11 @@ public class ProjectConfig {
         manager.createUser(user2);
 
         return manager;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
