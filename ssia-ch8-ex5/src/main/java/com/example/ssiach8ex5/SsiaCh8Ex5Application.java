6ddf058d6917ad8935fdabbc9d702e6dbf4565b4
yeop0740 <ssucgoo@gmail.com>
2023-09-28 16:53:32 +0900

    [feat] mvcMatcher 를 이용한 엔드포인트 권한 부여 구성

---
+++ ssia-ch8-ex5/src/main/java/com/example/ssiach8ex5/SsiaCh8Ex5Application.java
@@ -0,0 +1,13 @@
+package com.example.ssiach8ex5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh8Ex5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsiaCh8Ex5Application.class, args);
+    }
+
+}
---
+++ ssia-ch8-ex5/src/main/java/com/example/ssiach8ex5/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.ssiach8ex5.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!\n";
+    }
+
+}
