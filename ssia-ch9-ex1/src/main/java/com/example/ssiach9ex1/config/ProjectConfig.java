d74ca224d96e4d14e648686baa0a3f429370a4d0
yeop0740 <ssucgoo@gmail.com>
2023-10-01 21:48:29 +0900

    [feat] RequestValidationFilter 인증 필터 이전에 위치

---
+++ ssia-ch9-ex1/src/main/java/com/example/ssiach9ex1/config/ProjectConfig.java
@@ -0,0 +1,22 @@
+package com.example.ssiach9ex1.config;
+
+import com.example.ssiach9ex1.filter.RequestValidationFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.addFilterBefore(
+                new RequestValidationFilter(),
+                BasicAuthenticationFilter.class)
+                .authorizeRequests()
+                .anyRequest().permitAll();
+    }
+
+}
