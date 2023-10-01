34fd36dc8e34653836a527bb7c105aa97077c826
yeop0740 <ssucgoo@gmail.com>
2023-10-02 00:31:56 +0900

    [feat] 필터 필터 체인에 등록

---
+++ ssia-ch9-ex3/src/main/java/com/example/ssiach9ex3/config/ProjectConfig.java
@@ -0,0 +1,25 @@
+package com.example.ssiach9ex3.config;
+
+import com.example.ssiach9ex3.filter.AuthenticationLoggingFilter;
+import com.example.ssiach9ex3.filter.RequestValidationFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterBefore(
+                new RequestValidationFilter(),
+                BasicAuthenticationFilter.class)
+                .addFilterAfter(
+                        new AuthenticationLoggingFilter(),
+                        BasicAuthenticationFilter.class)
+                .authorizeRequests()
+                    .anyRequest()
+                        .permitAll();
+    }
+}
