7a8e4c9dd1d5285759128f0f3e45719e3a10dd81
yeop0740 <ssucgoo@gmail.com>
2023-10-01 23:50:32 +0900

    [feat] HelloController 생성

---
+++ ssia-ch9-ex3/src/main/java/com/example/ssiach9ex3/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.ssiach9ex3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello!\n";
+    }
+
+}
