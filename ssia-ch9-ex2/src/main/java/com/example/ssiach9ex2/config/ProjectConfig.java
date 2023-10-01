b5015585868046fa53347bbf1de38c987df45293
yeop0740 <ssucgoo@gmail.com>
2023-10-01 22:54:02 +0900

    [feat] BasicAuthenticationFilter 에 StaticAuthenticationFilter 적용

---
+++ ssia-ch9-ex2/src/main/java/com/example/ssiach9ex2/config/ProjectConfig.java
@@ -0,0 +1,23 @@
+package com.example.ssiach9ex2.config;
+
+import com.example.ssiach9ex2.filter.StaticKeyAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private StaticKeyAuthenticationFilter filter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.addFilterAt(filter,
+                BasicAuthenticationFilter.class)
+                .authorizeRequests()
+                .anyRequest().permitAll();
+    }
+}
