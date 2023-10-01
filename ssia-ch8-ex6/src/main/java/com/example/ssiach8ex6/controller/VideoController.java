f4d75b664fefa8fddd76470ebf2a7e632a14e6d8
yeop0740 <ssucgoo@gmail.com>
2023-10-01 13:29:37 +0900

    [feat] regexMatchers 를 이용하여 권한 부여 구성

---
+++ ssia-ch8-ex6/src/main/java/com/example/ssiach8ex6/controller/VideoController.java
@@ -0,0 +1,16 @@
+package com.example.ssiach8ex6.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VideoController {
+
+    @GetMapping("/video/{country}/{language}")
+    public String video(@PathVariable String country,
+                        @PathVariable String language) {
+        return "Video allowed for " + country + " " + language;
+    }
+
+}
